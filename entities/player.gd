eefe3d65c3e3a27eac426e758fd012f26c04bbf8
Camilo Ramírez <jucarave92@hotmail.com>
2025-07-17 13:11:21 -0500

    feat: draw reverse sprites
    Instead of changing the handinness of the character now the sprites
    are drawn in both directions to keep these details persistent

--- entities/player.gd
+++ entities/player.gd
@@ -7,6 +7,7 @@ var weapon: String = "gun"                  # This allows for changing weapon in
 var is_animation_busy: bool = false         # The animations are usually controlled by a function, when this variable is true it means that we are busy playing an animation that must override the rest
 var shooting_lower_delay: float = 0         # In seconds, how much time needs to pass to lower the gun after the last shot
 var projectile: Projectile = null           # Current projectile
+var flip_h: bool = false                    # Is the player looking to the left?
 
 @export var gravity: int = 10
 @export var movement_speed: int = 128
@@ -17,12 +18,19 @@ var projectile: Projectile = null           # Current projectile
 @onready var ceiling_raycast: ShapeCast2D = $CeilingRaycast
 @onready var projectile_spawn_point: Node2D = $ProjectileSpawnPoint
 
+func get_sprite_suffix() -> String:
+	var facing_direction: String = "r_"
+	if flip_h:
+		facing_direction = "l_"
+	
+	return facing_direction + weapon
+
 func process_movement() -> void:
 	var hor: float = Input.get_axis("left", "right")
 	
 	if hor != 0.0:
 		velocity.x = movement_speed * hor
-		sprite.flip_h = hor < 0;
+		flip_h = hor < 0;
 	else:
 		velocity.x = 0
 
@@ -44,14 +52,14 @@ func spawn_projectile() -> void:
 	projectile = projectile_scene.instantiate()
 	get_tree().root.add_child(projectile)
 	projectile.global_position = global_position
-	projectile.initialize(sprite.flip_h, projectile_spawn_point.position)
+	projectile.initialize(flip_h, projectile_spawn_point.position)
 	projectile.on_destroy.connect(on_projectile_destroy)
 
 func process_shoot() -> void:
 	if  Input.is_action_just_pressed("shoot"):
 		if shooting_lower_delay == 0 and is_idle():
 			is_animation_busy = true
-			sprite.play("idle_" + weapon + "_raise")
+			sprite.play("idle_" + get_sprite_suffix() + "_raise")
 		
 		if shooting_lower_delay > 0 or not is_idle():
 			spawn_projectile()
@@ -72,26 +80,26 @@ func process_sprite(delta: float) -> void:
 			
 			if is_idle():
 				is_animation_busy = true
-				sprite.play("idle_" + weapon + "_lower")
+				sprite.play("idle_" + get_sprite_suffix() + "_lower")
 				return
 	
 	if is_on_floor():
 		if velocity.x != 0:
-			sprite.play("run_" + weapon + sprite_suffix)
+			sprite.play("run_" + get_sprite_suffix() + sprite_suffix)
 		else:
-			sprite.play("idle_" + weapon + sprite_suffix)
+			sprite.play("idle_" + get_sprite_suffix() + sprite_suffix)
 	elif velocity.y > 0:
-		sprite.play("fall_" + weapon + sprite_suffix)
+		sprite.play("fall_" + get_sprite_suffix() + sprite_suffix)
 	elif velocity.y < 0:
-		sprite.play("jump_" + weapon + sprite_suffix)
+		sprite.play("jump_" + get_sprite_suffix() + sprite_suffix)
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	if sprite.animation == "idle_" + weapon + "_lower":
+	if sprite.animation == "idle_" + get_sprite_suffix() + "_lower":
 		is_animation_busy = false
-	elif sprite.animation == "idle_" + weapon + "_raise":
+	elif sprite.animation == "idle_" + get_sprite_suffix() + "_raise":
 		is_animation_busy = false
 		spawn_projectile()
-		sprite.play("idle_gun_aim")
+		sprite.play("idle_" + get_sprite_suffix() + "_aim")
 
 func _process(delta: float) -> void:
 	process_shoot()
