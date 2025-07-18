ce2ff570cd569c199f7300df23fc74b4e698f3ef
Camilo Ramírez <jucarave92@hotmail.com>
2025-07-18 00:54:03 -0500

    feat: add explosion particle effect when the bullet collides with a wall

--- entities/projectile.gd
+++ entities/projectile.gd
@@ -3,11 +3,14 @@ class_name Projectile
 
 @export var time_to_live: float = 0.6                 # In seconds, to prevent bullets flying across the map
 @export var velocity: float = 256
+@export var collision_particles_scene: PackedScene
 
 @onready var sprite: Sprite2D = $Sprite2D
 
 signal on_destroy()
 
+# Flips the sprite and sets its position relative to the
+# shooter depending if it's looking to the left or right
 func initialize(mirrored: bool, offset: Vector2) -> void:
 	sprite.flip_h = mirrored
 	global_position.y += offset.y
@@ -18,11 +21,21 @@ func initialize(mirrored: bool, offset: Vector2) -> void:
 	else:
 		global_position.x += offset.x
 
+# Creates a particle effect animation of the bullet exploding
+# to be called when it collides with something
+func spawn_collision_particles() -> void:
+	var particles: Node2D = collision_particles_scene.instantiate()
+	get_tree().root.add_child(particles)
+	particles.global_position = global_position
+
+# If someone is listening then let them know that this object
+# is to be destroyed
 func destroy() -> void:
 	on_destroy.emit()
 	queue_free()
 
 func _on_body_entered(_body: Node2D) -> void:
+	spawn_collision_particles()
 	destroy()
 
 func _process(delta: float) -> void:
