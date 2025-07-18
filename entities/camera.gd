82794098fe082dfa74a0682ab218ca9e61b47653
Camilo Ramírez <jucarave92@hotmail.com>
2025-07-17 23:13:21 -0500

    fix: camera subpixeling position
    It's not so good for pixel art games, so I'm round it's position
    on the screen

--- entities/camera.gd
+++ entities/camera.gd
@@ -13,13 +13,13 @@ func _physics_process(delta: float) -> void:
 	# Follow horizontally
 	var delta_h: float = player.global_position.x - global_position.x
 	if abs(delta_h) > HORIZONTAL_DEAD_ZONE:
-		global_position.x = move_toward(global_position.x, player.global_position.x, follow_speed * delta)
+		global_position.x = round(move_toward(global_position.x, player.global_position.x, follow_speed * delta))
 	
 	# Follow vertically
 	if player.is_on_floor():
 		last_platform_y = player.global_position.y
-		global_position.y = lerp(global_position.y, last_platform_y - VERTICAL_OFFSET, 0.1)
+		global_position.y = round(lerp(global_position.y, last_platform_y - VERTICAL_OFFSET, 0.1))
 	elif player.velocity.y > 0:
-		global_position.y = max(global_position.y, player.global_position.y - VERTICAL_OFFSET)
+		global_position.y = round(max(global_position.y, player.global_position.y - VERTICAL_OFFSET))
 	else:
-		global_position.y = lerp(global_position.y, last_platform_y - VERTICAL_OFFSET, 0.1)
+		global_position.y = round(lerp(global_position.y, last_platform_y - VERTICAL_OFFSET, 0.1))
